b10fcd7a71a016593ce3e10f3dc9a1799a1e49a3
pipi-shortstocking <jannie435@naver.com>
2025-05-24 15:55:43 +0900

    refactor: 포트폴리오 수정

--- controller/portfolioController.js
+++ controller/portfolioController.js
@@ -102,70 +102,60 @@ exports.updatePortfolio = async (req, res) => {
     const portfolioId = req.params.id;
     const { category, name, content } = req.body;
 
-    if (!category || !name || !content) {
-      await transaction.rollback();
-
-      // 파일 삭제
-      if (req.file) await fs.unlink(req.file.path);
-
-      return res.status(400).json({ error: '모든 필드를 입력해주세요.' });
-    }
-
-    // 로고 파일 체크
-    if (!req.file) {
-      await transaction.rollback();
-
-      // 파일 삭제
-      if (req.file) await fs.unlink(req.file.path);
-
-      return res.status(400).json({ error: '파일을 업로드 해주세요.' });
-    }
-
-    // 카테고리 검증
-    if (!Object.values(PortfolioCategory).includes(category)) {
-      await transaction.rollback();
-
-      // 파일 삭제
-      if (req.file) await fs.unlink(req.file.path);
-
-      return res.status(400).json({ error: '유효하지 않은 카테고리입니다.' });
-    }
-
     // 기존 포트폴리오 정보 조회
     const portfolio = await Portfolio.findByPk(portfolioId, {
       transaction,
     });
-
     if (!portfolio) {
       await transaction.rollback();
 
       // 파일 삭제
       if (req.file) await fs.unlink(req.file.path);
-
       return res.status(404).json({ error: '포트폴리오를 찾을 수 없습니다.' });
     }
 
-    // 기존 로고 파일 삭제
-    if (portfolio.logo) {
-      const oldFilePath = `uploads/${portfolio.logo}`;
-      await fs.unlink(oldFilePath);
+    // 수정할 값만 객체에 담기
+    if (category) {
+      // 카테고리 검증
+      if (!Object.values(PortfolioCategory).includes(category)) {
+        await transaction.rollback();
+
+        // 파일 삭제
+        if (req.file) await fs.unlink(req.file.path);
+        return res.status(400).json({ error: '유효하지 않은 카테고리입니다.' });
+      }
     }
 
-    // 파일 체크
-    const filePath = logoCheckFile(req.file);
+    const updateData = {
+      ...(category && { category }),
+      ...(name && { name }),
+      ...(content && { content }),
+    };
 
-    await Portfolio.update(
-      {
-        category,
-        name,
-        content,
-        logo: filePath,
-      },
-      {
-        where: { id: portfolioId },
-        transaction,
+    // 파일이 넘어온 경우 기존 파일 삭제 및 새 파일 경로 저장
+    if (req.file) {
+      // 기존 로고 파일 삭제
+      if (portfolio.logo) {
+        const oldFilePath = `uploads/${portfolio.logo}`;
+        await fs.unlink(oldFilePath);
       }
-    );
+
+      updateData.logo = logoCheckFile(req.file);
+    }
+
+    // 아무 값도 넘어오지 않은 경우
+    if (Object.keys(updateData).length === 0) {
+      await transaction.rollback();
+
+      // 파일 삭제
+      if (req.file) await fs.unlink(req.file.path);
+      return res.status(400).json({ error: '수정할 값을 입력해주세요.' });
+    }
+
+    await Portfolio.update(updateData, {
+      where: { id: portfolioId },
+      transaction,
+    });
 
     await transaction.commit();
 
