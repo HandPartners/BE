0dade14ae9cca0e6849cdcea270703437db0f5cf
pipi-shortstocking <jannie435@naver.com>
2025-05-25 13:12:30 +0900

    feat: 서버 설정 추가

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const cors = require('cors');
 const { sequelize } = require('./models');
 const http = require('http');
 const multer = require('multer');
